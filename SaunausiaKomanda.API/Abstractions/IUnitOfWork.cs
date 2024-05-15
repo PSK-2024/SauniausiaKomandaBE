0de015e8c803ef74ec778945c95c42bca0096641
Tedas <tedodarbo@gmail.com>
2024-05-15 22:42:51 +0300

    remove tag repo

--- SaunausiaKomanda.API/Abstractions/IUnitOfWork.cs
+++ SaunausiaKomanda.API/Abstractions/IUnitOfWork.cs
@@ -5,7 +5,6 @@ namespace SaunausiaKomanda.API.Abstractions
     public interface IUnitOfWork : IDisposable
     {
         IRecipeRepository Recipes { get; }
-        ITagRepository Tags { get; }
 
         Task SaveAsync(CancellationToken cancellationToken = default);
     }
