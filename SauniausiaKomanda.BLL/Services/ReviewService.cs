7ad5ed936acea7e80d56e3c74bc5e69baf599ce0
pminajevas <p.minajevas2002@gmail.com>
2024-05-25 23:51:05 +0300

    bug fix

--- SauniausiaKomanda.BLL/Services/ReviewService.cs
+++ SauniausiaKomanda.BLL/Services/ReviewService.cs
@@ -27,12 +27,34 @@ namespace SauniausiaKomanda.BLL.Services
         public async Task<DetailedRecipeResponseDTO> AddReviewAsync(AddReviewRequestDTO reviewToAdd)
         {
             var review = _mapper.Map<Review>(reviewToAdd);
-            review.UserId = (await _identityService.GetCurrentUser()).Id;
+            var user = await _identityService.GetCurrentUser();
+            review.UserId = user.Id;
             await _unitOfWork.Reviews.CreateAsync(review);
 
             await _unitOfWork.SaveAsync();
 
-            var result = _mapper.Map<DetailedRecipeResponseDTO>(await _unitOfWork.Recipes.GetAsync(x => x.Id == review.RecipeId));
+            var recipe = await _unitOfWork.Recipes.GetAsync(x => x.Id == review.RecipeId) ?? throw new Exception("Internal error");
+            var result = _mapper.Map<DetailedRecipeResponseDTO>(recipe);
+            foreach (var ingredient in recipe.Ingredients)
+            {
+                var ingredientGroup = result.Ingredients.Find(x => x.GroupName == ingredient.Group);
+                if (ingredientGroup != null)
+                {
+                    ingredientGroup.Items.Add(new DetailedRecipeIngredientItemsDTO { Name = ingredient.Description });
+                }
+                else
+                {
+                    ingredientGroup = new DetailedRecipeIngredientsResponseDTO { GroupName = ingredient.Group };
+                    ingredientGroup.Items.Add(new DetailedRecipeIngredientItemsDTO { Name = ingredient.Description });
+                    result.Ingredients.Add(ingredientGroup);
+                }
+            }
+
+            if (user.Favorites.Any(x => x.RecipeId == recipe.Id))
+            {
+                result.isFavorite = true;
+            }
+
             return result;
         }
     }
